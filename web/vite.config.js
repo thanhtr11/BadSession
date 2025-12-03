cfd7dfd9bf9579b1974899a8667438fa1e49a666
Your Name <your.email@example.com>
2025-12-03 13:22:09 +0700

    Use allowedHosts regex pattern to accept all domains

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: 'all',
+    allowedHosts: ['.'],
     proxy: {
       '/api': {
         target: 'http://server:9500',
