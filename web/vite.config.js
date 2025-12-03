653f16cc8b7e80912e66836e802ce9fe32fd0da1
Your Name <your.email@example.com>
2025-12-03 13:04:35 +0700

    Add custom domain to allowed hosts and fix proxy port to 9500

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,9 +6,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: ['bad.thanhtr.com', 'localhost', '127.0.0.1'],
     proxy: {
       '/api': {
-        target: 'http://server:8000',
+        target: 'http://server:9500',
         changeOrigin: true,
       }
     }
